4dfdf19efbd2bd3f2e85a617a1de69168e999d09
Mykolas <cheezus.fail@gmail.com>
2014-10-28 20:38:44 -0400

    commit test

--- Assets/Girl.cs
+++ Assets/Girl.cs
@@ -31,6 +31,8 @@ public class Girl : MonoBehaviour {
 	public const string MOOD_CONFUSED = "Confused";
 	public const string MOOD_AROUSED = "Aroused";
 
+	public const string MOOD_TEST = "Test";
+
 	public void IncrementHealth (float healthValue)
 	{
 		health += healthValue;
